e9e9b862d1c0ce5a80638214aad3d4481d466197
lillo X <lillox@rebeldia-lab.net>
2017-03-30 17:25:54 +0200

    Version 0.5 alpha See roadmap for info on changes

---
+++ aux.py
@@ -0,0 +1,113 @@
+"""Auxiliary module"""
+import colorama
+from colorama import Fore, Style
+import os
+import re
+
+"""Function ex_print prints a colorized text based on the message type"""
+
+
+def ex_print (type, msg, ret):
+	colorama.init()
+	# Define color and style constants
+	c_error = Fore.RED
+	c_action = Fore.YELLOW
+	c_ok = Fore.GREEN
+	c_white = Fore.WHITE
+	s_br = Style.BRIGHT
+	s_reset = Style.RESET_ALL
+	message = {
+		"error": c_error + s_br,
+		"action": c_action,
+		"positive": c_ok + s_br,
+		"info": c_white + s_br,
+		"reset": s_reset
+	}
+	style = message.get(type, s_reset)
+	if ret == 0:
+		print(style + msg, end = "")
+	else:
+		print(style + msg)
+
+
+def banner (version):
+	os.system('clear')
+	ex_print('error', " ____             _            ______        ___   _", 1)
+	ex_print('error', "|  _ \ ___  _   _| |_ ___ _ __|  _ \ \      / / \ | |", 0)
+	ex_print('action', '\tRouter PWN tool', 1)
+	ex_print('error', "| |_) / _ \| | | | __/ _ \ '__| |_) \ \ /\ / /|  \| |", 0)
+	ex_print('action', '\t(C) 2017 Domenico "LilloX" Malorni', 1)
+	ex_print('error', '|  _ < (_) | |_| | ||  __/ |  |  __/ \ V  V / | |\  |', 0)
+	ex_print('action', '\tlillox[at]rebeldia-lab[dot]net', 1)
+	ex_print('error', '|_| \_\___/ \__,_|\__\___|_|  |_|     \_/\_/  |_| \_|', 0)
+	ex_print('action', '\thttps://www.domenicomalorni.eu', 1)
+	ex_print('action', 'Version :' + version, 1)
+	ex_print('info',
+	         'Legal disclaimer: Usage of RouterPWN for attacking targets without prior mutual consent is illegal. It is the responsibility of end user to obey all applicable local, state and federal laws.\nDevelopers assume no liability and are not responsible for any misuse or damage caused by this program',
+	         1)
+	ex_print('reset', '', 1)
+
+
+"""Functions to detect the router brand/model"""
+
+
+def is_netgear (header):
+	try:
+		realm = (header['WWW-Authenticate'])
+		model = re.search('Basic realm="(.+?)"', realm).group(1)
+	except:
+		try:
+			model = re.search('Digest realm="(.+?)Authentication"', realm).group(1)
+			with open('minchia', 'a') as out_temp:
+				out_temp.writelines('\n' + header)
+			out_temp.close()
+		except:
+			try:
+				realm = (header['WWW-Authenticate'])
+				model = re.search('Basic realm=(.*)', realm).group(1)
+			except:
+				return ''
+	model = str(model).replace(' ', '_')
+	model = model.upper()
+	return model
+
+
+# Check if is a D-Link router
+def is_dlink (body):
+	global model
+	try:
+		title = body.select("title")
+	except:
+		return 0
+	else:
+		if str(title).upper().find("D-LINK") != "-1":
+			# First case: the model name is inside a div of class pp
+			try:
+				temp = body.find("div", attrs = {'class': 'pp'})
+				model = re.search(': (.+?)<a', str(temp)).group(1)
+				model = 'D-Link ' + model
+			except:
+				# Second case: the model name is inside a table of class versionTable
+				try:
+					temp = body.find("table", attrs = {'class': 'versionTable'})
+					t2 = temp.find_all("td")
+					model = re.search(' : (.+?)<', str(t2[0])).group(1)
+					model = 'D-Link ' + model
+				except:
+					# Third case: model name is in a variable inside a javascript
+					try:
+						test = str(body)
+						model = re.search("ModemVer='(.+?)'", test).group(1)
+						model = 'D-Link ' + model
+					except:
+						return 0
+			return 1
+		else:
+			return 0
+
+
+def file_len (fname):
+	with open(fname) as f:
+		for i, l in enumerate(f):
+			pass
+	return i + 1
---
+++ routerPwn.py
@@ -0,0 +1,63 @@
+import argparse
+import classes
+import aux
+import sys
+import requests
+
+global __version__
+__version__ = '0.5 alpha'
+__author__ = 'Domenico "LilloX" Malorni'
+script_name = sys.argv[0]
+
+parser = argparse.ArgumentParser(description = 'Two operational modes: Signle IP\IP List',usage = aux.banner(__version__),
+                                 epilog = 'Examples:\nSingle ip check and output to file:\n\t'+script_name+' -i 192.168.0.1 -o out.txt\nCheck list of ip and write results on the test.out file'+
+                                 '\n\t'+script_name+' -l ip_list.txt -o test.out\n\nYou have to specify -i OR -l. Output use is optional',formatter_class=argparse.RawTextHelpFormatter)
+group = parser.add_mutually_exclusive_group(required = True)
+group.add_argument("-i", '--ip', help = "Single ip to test. No protocol or port", dest = "input_ip")
+group.add_argument("-l", '--list', help = "List of ip to test. No protocol or port", dest = "input_file")
+parser.add_argument("-o", "--output", help = "Output credentials found to file ", dest = "output_file")
+args = parser.parse_args()
+
+opt = classes.Options
+
+if args.input_ip:
+	opt.single_ip = True
+	opt.list_mode = False
+	opt.ip = args.input_ip
+
+if args.input_file:
+	opt.single_ip = False
+	opt.list_mode = True
+	opt.list_file = args.input_file
+
+if args.output_file:
+	opt.o_file = True
+	opt.o_file_name = args.output_file
+
+
+
+aux.banner(__version__)
+if opt.single_ip:
+	router = classes.Router(opt)
+	del router
+else:
+	i = 0
+	with open(opt.list_file,'r') as ip_list:
+		for line in ip_list:
+			i += 1
+			line = line.replace("\n","")
+			opt.ip = line
+			aux.ex_print("info","Processing IP number "+ str(i) + " of "+ str(aux.file_len(opt.list_file)),1)
+			router = classes.Router(opt)
+			router.open_ports=[]
+			# Write to outfile the results even the router is not exploitable
+			if opt.o_file:
+				output = open(opt.o_file_name, 'a')
+				if router.vulnerable:
+					output.writelines(router.model+','+router.ip+','+router.port+','+str(router.vulnerable)+','+router.exploit+','+router.username+','+router.password+'\n')
+				else:
+					if router.model != '':
+						output.writelines(router.model+','+router.ip+','+router.port+','+router.exploit+'\n')
+			del router
+	output.close()
+	ip_list.close()
